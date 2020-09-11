c1300f7cf79540bc16580e74668898cebcf0cbea
Kami-PM08 <37862210+Kami-PM08@users.noreply.github.com>
2020-09-11 13:00:30 -0500

    Primer commit.

---
+++ server/sockets/socket.js
@@ -0,0 +1,50 @@
+const { io } = require('../server');
+
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utils/utils');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (usuario, callback) => {
+
+        console.log(usuario);
+
+        if (!usuario.nombre || !usuario.sala) {
+            return callback({
+                error: true,
+                message: 'El nombre/sala es necesario.'
+            });
+        }
+
+        client.join(usuario.sala);
+
+        usuarios.agregarPersona(client.id, usuario.nombre, usuario.sala);
+        let personas = usuarios.getPersonasSala(usuario.sala);
+        client.broadcast.to(usuario.sala).emit('listaPersonas', personas);
+
+        callback(personas);
+    });
+
+    client.on('crearMensaje', (data) => {
+        let persona = usuarios.getPersona(client.id);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    });
+
+    client.on('mensajePrivado', (data) => {
+        let persona = usuarios.getPersona(client.id);
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    });
+
+    client.on('disconnect', () => {
+        let borrado = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(borrado.sala).emit('crearMensaje', crearMensaje('Admin', `${borrado.nombre} ha abandonado el chat.`));
+
+        client.broadcast.to(borrado.sala).emit('listaPersonas', usuarios.getPersonasSala(borrado.sala));
+
+    });
+});
